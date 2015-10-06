9d7d4cfeb1883fb70fd293b54aef82e207f53a5c
lb90 <luca90music@hotmail.it>
2015-10-06 20:29:45 +0000

    added audio devices and api discovery and logging (proof of concept)

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <list>
 #include <string>
-#include <function>
+#include <functional>
 #include <exception>
-#include <boost/lockfree/spsc_queue>
-#include <boost/program_options>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <boost/program_options.hpp>
 #include <portaudio.h>
 #include <gtk/gtk.h>
+using namespace std;
 namespace po = boost::program_options;
 
 /* options:
@@ -27,7 +28,7 @@ int bit_depth;
 int channels;
 enum {} api;
 
-/* a frame is
+/* a frame is, in interleaved pcm audio, a sequence of samples for each channel */
 typedef int framecount_t;
 framecount_t buffer_size;
 framecount_t fragment_size;
@@ -41,11 +42,22 @@ int framesize;
 
 std::list<std::string> song_list;
 
-boost::spsc_queue<char> buffer;
+//boost::spsc_queue<char> buffer;
 
-Pa_Stream *stream;
+PaStream *stream;
 
 GtkWidget *window;
+
+/* function prototypes */
+void init_log(void);
+void init_options(void);
+void init_gui(void);
+void init_portaudio(void);
+void enumerate_devices_by_api(int api_idx);
+
+void close_portaudio(void);
+void close_log(void);
+/* */
 
 int main(int argc, char **argv) {
 	init_log();
@@ -93,25 +105,39 @@ void init_gui(void) {
 }
 
 void init_portaudio(void) {
-	pa_Error ret =
-	Pa_Inititalize();
-	if (ret != pa_NoError) {
+	PaError ret =
+	Pa_Initialize();
+	if (ret != paNoError) {
 		cerr << "Portaudio error: " << Pa_GetErrorText(ret) << "\n";
 		exit(EXIT_FAILURE);
 	}
 
-	ret =
-	if (ret != pa_NoError) {
-		cerr << "Portaudio error: " << Pa_GetErrorText(ret) << "\n";
+	int api_max = Pa_GetHostApiCount();
+	if (api_max < 0) { /* if api_max is negative it is actually an error code */
+		cerr << "Portaudio error: " << Pa_GetErrorText(api_max) << "\n";
 		exit(EXIT_FAILURE);
 	}
-	clog << "founded devices:\n";
-
+	if (api_max == 0) {
+		cerr << "Error: there is no software for audio output" << "\n";
+		exit(EXIT_FAILURE);
+	}
+	/* container here must be non intrusive, as api_info are internal to portaudio */
+	std::vector<const char*> api_names;
+	for (int api_idx = 0; api_idx < api_max; api_idx++) {
+		const PaHostApiInfo* api_info = Pa_GetHostApiInfo(api_idx);
+		api_names.push_back(api_info->name);
+	}
+	clog << "the system has support for:\n";
+		for (auto& an : api_names) clog << " - " << an << "\n";
+	for (int api_idx = 0; api_idx < api_max; api_idx++) {
+		enumerate_devices_by_api(api_idx);
+	}
+/*
 	PaStreamParameters outputparam;
 	outputparam
 	ret =
 	Pa_OpenStream(&stream, , , (double) sample_rate, paFramesPerBufferUnspecified, , , NULL);
-	if (ret != pa_NoError) {
+	if (ret != paNoError) {
 		cerr << "Portaudio error: " << Pa_GetErrorText(ret) << "\n";
 		exit(EXIT_FAILURE);
 	}
@@ -119,7 +145,24 @@ void init_portaudio(void) {
 	clog << " - sample rate: " << sample_rate << " hz." << "\n";
 	clog << " - bit depth: " << bit_depth << " bit." << "\n";
 	clog << " - channels: " << channels << "." << "\n";
+*/
 	clog << std::endl;
+}
+
+void enumerate_devices_by_api(int api_idx) {
+	const PaHostApiInfo* api_info = Pa_GetHostApiInfo(api_idx);
+	clog << "detected devices for " << api_info->name << "\n";
+
+	for (int device_idx_api_relative = 0;
+	         device_idx_api_relative < api_info->deviceCount;
+	         ++device_idx_api_relative)
+	{
+		int device_idx_absolute = Pa_HostApiDeviceIndexToDeviceIndex(api_idx, device_idx_api_relative);
+		const PaDeviceInfo *device_info = Pa_GetDeviceInfo(device_idx_absolute);
+		clog << device_idx_api_relative+1 /* count */ << ": " << device_info->name << "\n";
+		clog << "default sample rate: " << device_info->defaultSampleRate << " hz\n";
+		clog << "number of output channels: " << device_info->maxOutputChannels << "\n";
+	}
 }
 
 void process(void* out, framecount_t framecount) {
@@ -129,9 +172,12 @@ void process(void* out, framecount_t framecount) {
 void callback(void) {
 }
 
+void background(void) {
+}
+
 void update_ui(void) {
 
-	wait(update_ui_rate);
+	//wait(update_ui_rate);
 }
 
 void close_portaudio(void) {
